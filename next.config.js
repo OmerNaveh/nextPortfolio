252fd3955db58188e994deefd92c8819d56bdb3c
Omer Naveh <omernaveh@Omers-MacBook-Pro.local>
2023-11-14 15:15:27 +0200

    removed gh pages deployment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = (process.env.NODE_ENV = "production");
-
 const nextConfig = {
-  output: "export",
-  distDir: "dist",
-  basePath: isProd ? "/nextPortfolio" : "",
-  assetPrefix: isProd ? "/nextPortfolio/" : "",
   images: {
     remotePatterns: [
       {
